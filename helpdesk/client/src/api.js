21cd1e34027cfff16b2aa548f031d693c92b4197
sandipto729 <sandipto729@gmail.com>
2025-08-02 09:36:09 +0530

    Update server base URL and add a root route response

--- helpdesk/client/src/api.js
+++ helpdesk/client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json'
   }
